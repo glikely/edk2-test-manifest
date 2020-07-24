c011dfc68526e98f8f5fae06765bd79deaeb8d75
Grant Likely <grant.likely@arm.com>
2020-07-24 11:03:28 +0100

    Build the UEFI shell
    The UEFI shell is needed by the SCT; build it here also
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 # Build the SCT
 export CROSS_COMPILE=aarch64-linux-gnu-
 ./SctPkg/build.sh AARCH64 GCC RELEASE
+
+# Build the UEFI shell
+export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+cd edk2
+source ./edksetup.sh
+make -C BaseTools/
+build -a AARCH64 -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
+cd ..
+unset GCC5_AARCH64_PREFIX
 
 # Put some version information into the build directory
 cat > ./Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64/versions.txt << EOF
@@ -11,4 +21,4 @@ EDK2_TEST_VER=`git -C ./edk2-test describe`
 EOF
 
 # Zip up the image
-zip -r edk2-test-aarch64.zip Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64
+zip -r edk2-test-aarch64.zip Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64 edk2/Build/Shell/RELEASE_GCC5/AARCH64/*.efi
