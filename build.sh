e71ca55803c2a0a5cfea2faf4568cb4bf3b8218c
Grant Likely <grant.likely@arm.com>
2020-07-24 10:04:31 +0100

    Create zip file of SCT
    Adds a file containing version information and creates a zip file of the built SCT tests that can be unzipped onto a filesystem.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# Build the SCT
 export CROSS_COMPILE=aarch64-linux-gnu-
 ./SctPkg/build.sh AARCH64 GCC RELEASE
+
+# Put some version information into the build directory
+cat > ./Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64/versions.txt << EOF
+EDK2_VER=`git -C ./edk2 describe`
+EDK2_TEST_VER=`git -C ./edk2-test describe`
+EOF
+
+# Zip up the image
+zip -r edk2-test-aarch64.zip Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64
