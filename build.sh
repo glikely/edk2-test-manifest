f439cb3f71a3b25b541e0f77b4907b302bb712dd
Grant Likely <grant.likely@arm.com>
2020-08-21 13:00:21 +0100

    Add build for 32-bit ARM
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- build.sh
+++ build.sh
@@ -5,35 +5,73 @@ export EDK_TOOLS_PATH=
 export CONF_PATH=
 export WORKSPACE=$PWD
 export PACKAGES_PATH=$WORKSPACE/edk2
-export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
-export CROSS_COMPILE=aarch64-linux-gnu-
 
 source ./edk2/edksetup.sh
 
 getconf _NPROCESSORS_ONLN
 NUM_CPUS=$((`getconf _NPROCESSORS_ONLN` + 2))
 
-# Build the UEFI shell
 make -C edk2/BaseTools/
+
+###########
+# AARCH64
+
+export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+
+# Build the UEFI shell
 build -n $NUM_CPUS -a AARCH64 -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
 
 # Build the SCT
 ./SctPkg/build.sh AARCH64 GCC RELEASE
 
 # Assemble all the files that need to be in the zip file
-mkdir -p TEST_ESP/EFI/BOOT
-cp Build/Shell/RELEASE_GCC5/AARCH64/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi TEST_ESP/EFI/BOOT/BOOTAA64.efi
-mkdir -p TEST_ESP/SCT
-cp -r Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64/* TEST_ESP/SCT/
-cp Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/SctStartup.nsh TEST_ESP/Startup.nsh
+mkdir -p AARCH64_SCT/EFI/BOOT
+cp Build/Shell/RELEASE_GCC5/AARCH64/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi AARCH64_SCT/EFI/BOOT/BOOTAA64.efi
+
+mkdir -p AARCH64_SCT/SCT
+cp -r Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64/* AARCH64_SCT/SCT/
+cp Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/SctStartup.nsh AARCH64_SCT/Startup.nsh
 
 # Put some version information into the ESP directory
-cat > ./TEST_ESP/versions.txt << EOF
+cat > ./AARCH64_SCT/versions.txt << EOF
 EDK2_VER=`git -C ./edk2 describe`
 EDK2_TEST_VER=`git -C ./edk2-test describe`
 BUILD_DATE="`date`"
 EOF
 
 # Zip up the test folder
-cd TEST_ESP
+cd AARCH64_SCT
 zip -r ../edk2-test-aarch64.zip *
+cd ..
+
+###########
+# ARM
+
+export GCC5_ARM_PREFIX=arm-linux-gnueabi-
+
+# Build the UEFI shell
+build -n $NUM_CPUS -a ARM -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
+
+# Build the SCT
+./SctPkg/build.sh ARM GCC RELEASE
+
+# Assemble all the files that need to be in the zip file
+mkdir -p ARM_SCT/EFI/BOOT
+cp Build/Shell/RELEASE_GCC5/ARM/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi ARM_SCT/EFI/BOOT/BOOTARM.efi
+
+mkdir -p ARM_SCT/SCT
+cp -r Build/UefiSct/RELEASE_GCC5/SctPackageARM/ARM/* ARM_SCT/SCT/
+cp Build/UefiSct/RELEASE_GCC5/SctPackageARM/SctStartup.nsh ARM_SCT/Startup.nsh
+
+# Put some version information into the ESP directory
+cat > ./ARM_SCT/versions.txt << EOF
+EDK2_VER=`git -C ./edk2 describe`
+EDK2_TEST_VER=`git -C ./edk2-test describe`
+BUILD_DATE="`date`"
+EOF
+
+# Zip up the test folder
+cd ARM_SCT
+zip -r ../edk2-test-arm.zip *
+cd ..
+
