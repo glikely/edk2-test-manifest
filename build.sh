ed1a5b3dbb3146390d90f78329f84fb59aa06fc6
Stuart Yoder <stuart.yoder@arm.com>
2020-09-01 11:11:09 -0500

    edk2-test-manifest: add argument to select ARM or AARCH64
    build.sh unconditionally builds SCT both ARM (32-bit) and AARCH64, which
    means all users must have both cross compilers. Improve the usability by
    making the build target a command line option. Default will be AARCH64.
    
    Also, refactor common code between the builds and use a $BUILD_ARCH
    identify which arch is being built. Script now build only one target at
    a time instead of both.
    
    Signed-off-by: Stuart Yoder <stuart.yoder@arm.com>
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
 
+if [[ $# -eq 0  ]]
+then
+	BUILD_ARCH=AARCH64
+elif [[ $# -eq 1  ]]
+then
+	BUILD_ARCH=$1
+fi
+
+case $BUILD_ARCH in
+	ARM)
+		export GCC5_ARM_PREFIX=arm-linux-gnueabi-
+		export CROSS_COMPILE_32=arm-linux-gnueabi-
+		BOOT_IMAGE_NAME=BOOTARM.efi;;
+	AARCH64)
+		export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+		export CROSS_COMPILE_64=aarch64-linux-gnu-
+		BOOT_IMAGE_NAME=BOOTAA64.efi;;
+	*)
+		echo  "Usage: build.sh [ARM|AARCH64]"
+		exit
+esac
+
+# clear all positional parameters
+set --
+
 export EDK_TOOLS_PATH=
 export CONF_PATH=
 export WORKSPACE=$PWD
@@ -13,67 +38,29 @@ NUM_CPUS=$((`getconf _NPROCESSORS_ONLN` + 2))
 
 make -C edk2/BaseTools/
 
-###########
-# AARCH64
-
-export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
-export CROSS_COMPILE_64=aarch64-linux-gnu-
-
 # Build the UEFI shell
-build -n $NUM_CPUS -a AARCH64 -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
+build -n $NUM_CPUS -a $BUILD_ARCH -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
 
 # Build the SCT
-./SctPkg/build.sh AARCH64 GCC RELEASE
+./SctPkg/build.sh $BUILD_ARCH GCC RELEASE
 
 # Assemble all the files that need to be in the zip file
-mkdir -p AARCH64_SCT/EFI/BOOT
-cp Build/Shell/RELEASE_GCC5/AARCH64/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi AARCH64_SCT/EFI/BOOT/BOOTAA64.efi
+mkdir -p ${BUILD_ARCH}_SCT/EFI/BOOT
+cp Build/Shell/RELEASE_GCC5/$BUILD_ARCH/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi ${BUILD_ARCH}_SCT/EFI/BOOT/$BOOT_IMAGE_NAME
 
-mkdir -p AARCH64_SCT/SCT
-cp -r Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/AARCH64/* AARCH64_SCT/SCT/
-cp Build/UefiSct/RELEASE_GCC5/SctPackageAARCH64/SctStartup.nsh AARCH64_SCT/Startup.nsh
+mkdir -p ${BUILD_ARCH}_SCT/SCT
+cp -r Build/UefiSct/RELEASE_GCC5/SctPackage${BUILD_ARCH}/$BUILD_ARCH/* ${BUILD_ARCH}_SCT/SCT/
+cp Build/UefiSct/RELEASE_GCC5/SctPackage${BUILD_ARCH}/SctStartup.nsh ${BUILD_ARCH}_SCT/Startup.nsh
 
 # Put some version information into the ESP directory
-cat > ./AARCH64_SCT/versions.txt << EOF
+cat > ./${BUILD_ARCH}_SCT/versions.txt << EOF
 EDK2_VER=`git -C ./edk2 describe`
 EDK2_TEST_VER=`git -C ./edk2-test describe`
 BUILD_DATE="`date`"
 EOF
 
 # Zip up the test folder
-cd AARCH64_SCT
-zip -r ../edk2-test-aarch64.zip *
-cd ..
-
-###########
-# ARM
-
-export GCC5_ARM_PREFIX=arm-linux-gnueabi-
-export CROSS_COMPILE_32=arm-linux-gnueabi-
-
-# Build the UEFI shell
-build -n $NUM_CPUS -a ARM -t GCC5 -p ShellPkg/ShellPkg.dsc -b RELEASE
-
-# Build the SCT
-./SctPkg/build.sh ARM GCC RELEASE
-
-# Assemble all the files that need to be in the zip file
-mkdir -p ARM_SCT/EFI/BOOT
-cp Build/Shell/RELEASE_GCC5/ARM/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi ARM_SCT/EFI/BOOT/BOOTARM.efi
-
-mkdir -p ARM_SCT/SCT
-cp -r Build/UefiSct/RELEASE_GCC5/SctPackageARM/ARM/* ARM_SCT/SCT/
-cp Build/UefiSct/RELEASE_GCC5/SctPackageARM/SctStartup.nsh ARM_SCT/Startup.nsh
-
-# Put some version information into the ESP directory
-cat > ./ARM_SCT/versions.txt << EOF
-EDK2_VER=`git -C ./edk2 describe`
-EDK2_TEST_VER=`git -C ./edk2-test describe`
-BUILD_DATE="`date`"
-EOF
-
-# Zip up the test folder
-cd ARM_SCT
-zip -r ../edk2-test-arm.zip *
+cd ${BUILD_ARCH}_SCT
+zip -r ../edk2-test-${BUILD_ARCH}.zip *
 cd ..
 
